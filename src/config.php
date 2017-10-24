2bbfe094fc29c32f1496070bb8b314995e75e53c
siriusSupreme <guoplc@163.com>
2017-10-24 17:54:40 +0800

    ''

--- src/config.php
+++ src/config.php
@@ -60,13 +60,13 @@ return [
     | sources which represent each model / table. These sources may then
     | be assigned to any extra authentication guards you have defined.
     |
-    | Supported: "database", "eloquent"
+    | Supported: "database", "model"
     |
     */
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'model',
             'model' => App\User::class,
         ],
 
